012b4ea19c7f3351f3be4ed2c3db94c2e7e1014c
richcountrylife-commits <rich.country.life@gmail.com>
2026-07-23 11:56:40 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ============================================
 window.APP_CONFIG = {
   // Google Apps ScriptをWebアプリとしてデプロイした時のURL
-  GAS_URL: "https://script.google.com/macros/s/AKfycbzdk5Ha08S2goSZbfY5exBqtzmyBY9VKCkxTLtMHN0-X5mkULJZ3C7f4yfBzrXqgk1f/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwcDJnLDQaQciAlTWihW7DkawB3XtdTv6rzHb1scTCzO_6SkjmTH3OKp99AWyolM3s5/exec",
 
   // ログイン用パスコード（4〜6桁の数字推奨。お好きな数字に変更してください）
   PASSCODE: "0708"
