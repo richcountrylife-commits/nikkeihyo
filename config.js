baaa84de65077685d2c3490f003132587ad52985
richcountrylife-commits <rich.country.life@gmail.com>
2026-07-29 12:39:39 +0900

    Update PASSCODE in config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ window.APP_CONFIG = {
  GAS_URL: "https://script.google.com/macros/s/AKfycbwcDJnLDQaQciAlTWihW7DkawB3XtdTv6rzHb1scTCzO_6SkjmTH3OKp99AWyolM3s5/exec",
 
   // ログイン用パスコード（4〜6桁の数字推奨。お好きな数字に変更してください）
-  PASSCODE: "358"
+  PASSCODE: "0708"
 };
